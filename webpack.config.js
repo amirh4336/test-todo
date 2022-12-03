0849bd0a648fea7335a8912b694eafb2c86f6b2e
amir <amirhossienheydarpoor@gmail.com>
2022-12-03 16:41:30 +0330

    add sass to project

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+
+
+module.exports = {
+
+    module : {
+        rules : [
+            {
+                
+                test : /\.s[ac]ss$/,
+                use : ['style-loader', 'css-loader' , 'sass-loader']
+              },
+
+        ]
+    },
+
+}
